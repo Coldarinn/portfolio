0961be818cdf459dfb8da1dad08e787e78772026
palkin.kirill <kirilpalkin@yandex.ru>
2024-05-09 14:48:14 +0300

    smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { Scrollbar } from "@/components/Scrollbar"
 import { IntroAsync } from "@/components/Intro"
 import { Cursor } from "@/components/Cursor"
 import { SkillsAsync } from "./components/Skills"
-import { useGSAP } from "@gsap/react"
-// import { MatrixBackground } from "@/components/MatrixBackground"
-import gsap from "gsap"
-import { ScrollToPlugin } from "gsap/ScrollToPlugin"
+import LocomotiveScroll from "locomotive-scroll"
+import "locomotive-scroll/locomotive-scroll.css"
+
+new LocomotiveScroll({
+  smooth: true,
+});
 
 export const App = () => {
-  // useSmoothScroll()
-
   return (
     <>
       <Suspense fallback={<Preloader />}>
@@ -21,32 +21,6 @@ export const App = () => {
         <Cursor />
         <Scrollbar />
       </Suspense>
-
-      {/* <MatrixBackground /> */}
     </>
   )
-}
-
-const useSmoothScroll = () => {
-  useGSAP(() => {
-    gsap.registerPlugin(ScrollToPlugin)
-    const handleScroll = (event: WheelEvent) => {
-      event.preventDefault()
-
-      const delta = Math.max(-1, Math.min(1, event.deltaY || -event.detail))
-      const speed = 1.5
-
-      gsap.to(window, {
-        duration: speed,
-        scrollTo: { y: window.scrollY + delta * 250 },
-        ease: "power4.out",
-      })
-    }
-
-    window.addEventListener("wheel", handleScroll)
-
-    return () => {
-      window.removeEventListener("wheel", handleScroll)
-    }
-  }, [])
 }
